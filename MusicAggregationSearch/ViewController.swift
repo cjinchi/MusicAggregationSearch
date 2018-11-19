aa89b2c6382cb5e7b37caf8f7f8a0aafeb62808e
cjinchi <cjinchi@qq.com>
2018-11-19 20:56:06 +0800

    Initial Commit

---
+++ MusicAggregationSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicAggregationSearch
+//
+//  Created by 李秋霖 on 2018/11/19.
+//  Copyright © 2018 com.cjinchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
